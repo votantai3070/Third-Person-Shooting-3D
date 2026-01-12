84f614eff3d4b4177f09984df0d43c92a64a16d3
votantai3070 <votantai.07092002@gmail.com>
2026-01-12 21:44:03 +0700

    Automatic Generation Map

--- Assets/_Scripts/LevelGeneration/LevelGenerator.cs
+++ Assets/_Scripts/LevelGeneration/LevelGenerator.cs
@@ -3,9 +3,52 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
+    [SerializeField] private Transform lastLevelPart;
     [SerializeField] private List<Transform> levelParts;
+    private List<Transform> currentLevelParts;
     [SerializeField] private SnapPoint nextSnapPoint;
 
+
+    [Space]
+    [SerializeField] private float generationCooldown;
+    private float cooldownTimer;
+    private bool generationOver;
+
+    private void Start()
+    {
+        currentLevelParts = new List<Transform>(levelParts);
+    }
+
+    private void Update()
+    {
+        if (generationOver)
+            return;
+
+        cooldownTimer -= Time.deltaTime;
+
+        if (cooldownTimer <= 0f)
+        {
+            if (currentLevelParts.Count > 0)
+            {
+                cooldownTimer = generationCooldown;
+                GenerationNextLevelPart();
+            }
+            else if (generationOver == false)
+            {
+                FinishGeneration();
+            }
+        }
+    }
+
+    private void FinishGeneration()
+    {
+        generationOver = true;
+
+        Transform levelPart = Instantiate(lastLevelPart);
+        LevelPart lp = levelPart.GetComponent<LevelPart>();
+
+        lp.SnapAndAlignPartTo(nextSnapPoint);
+    }
 
     [ContextMenu("Generate Next Level Part")]
     private void GenerationNextLevelPart()
@@ -20,11 +63,11 @@ public class LevelGenerator : MonoBehaviour
 
     private Transform ChooseRandomPart()
     {
-        int randomIndex = Random.Range(0, levelParts.Count);
+        int randomIndex = Random.Range(0, currentLevelParts.Count);
 
-        Transform choosePart = levelParts[randomIndex];
+        Transform choosePart = currentLevelParts[randomIndex];
 
-        levelParts.RemoveAt(randomIndex);
+        currentLevelParts.RemoveAt(randomIndex);
 
         return choosePart;
     }
