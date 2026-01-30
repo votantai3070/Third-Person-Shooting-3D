23cc4adab05a3c21c8362bb40817804f30309c17
votantai3070 <votantai.07092002@gmail.com>
2026-01-30 16:19:43 +0700

    Add Comic UI

---
+++ Assets/_Scripts/UI/UI_ComicPanel.cs
@@ -0,0 +1,60 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UI_ComicPanel : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField] private Image[] comicPanel;
+    [SerializeField] private int imageIndex;
+    [SerializeField] private GameObject playButton;
+
+    private Image myImage;
+    [SerializeField] private bool comicShowOver;
+    private bool isFadeIn;
+
+    [SerializeField] float fadeDuration = 1.5f;
+
+    private void Start()
+    {
+        foreach (var panel in comicPanel)
+        {
+            Color color = panel.color;
+            color.a = 0f;
+            panel.color = color;
+        }
+
+        ShowNextPanel();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (comicShowOver || isFadeIn) return;
+        ShowNextPanel();
+    }
+
+    private void ShowNextPanel()
+    {
+        if (imageIndex >= comicPanel.Length)
+        {
+            comicShowOver = true;
+            playButton.SetActive(true);
+            return;
+        }
+
+        isFadeIn = true;
+
+        comicPanel[imageIndex].DOFade(1f, fadeDuration).SetUpdate(true).OnComplete(() =>
+        {
+            isFadeIn = false;
+            imageIndex++;
+
+            if (imageIndex >= comicPanel.Length)
+            {
+                comicShowOver = true;
+                playButton.SetActive(true);
+                return;
+            }
+        });
+    }
+}
